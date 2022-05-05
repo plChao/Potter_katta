18f24a3fee9d457f7151f85783f0c685f827620e
plchao <plchao0415@gmail.com>
2022-05-05 15:00:18 +0800

    [Potter.py][5/5 pass] restruct the code.

--- Potter.py
+++ Potter.py
@@ -8,16 +8,6 @@ class Potter:
     def discountprice(self, basket):
         if len(set(basket)) != len(basket):
             print('Error: only accept unique basket.')
-        if len(basket) == 0:
-            return 0
-        elif len(basket) == 1:
-            return 8
-        elif len(basket) == 2:
-            return 8*2*0.95
-        elif len(basket) == 3:
-            return 8*3*0.9
-        elif len(basket) == 4:
-            return 8*4*0.8
-        else:
-            return 8*5*0.75
+        discount = {0:1, 1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
+        return 8*len(basket)*discount[len(basket)]
         
