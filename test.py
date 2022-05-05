c33de0eadb6e0be4d761fc236f0044c9ba71f2ce
plchao <plchao0415@gmail.com>
2022-05-05 14:50:41 +0800

    [test.py][1/5 pass] add four discount tests.

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import imp
 import unittest
 from Potter import Potter
 
@@ -12,5 +11,26 @@ class TestPotter(unittest.TestCase):
         assert (8 == self.test_object.price([3]))
         assert (8 == self.test_object.price([4]))
         assert (8 * 3 == self.test_object.price([1, 1, 1]))
+    def testSimpleDiscounts1(self):
+        assert (8 * 2 * 0.95 == self.test_object.price([0, 1]))
+    def testSimpleDiscounts2(self):
+        assert (8 * 3 * 0.9 == self.test_object.price([0, 2, 4]))
+    def testSimpleDiscounts3(self):
+        assert (8 * 4 * 0.8 == self.test_object.price([0, 1, 2, 4]))
+    def testSimpleDiscounts4(self):
+        assert (8 * 5 * 0.75 == self.test_object.price([0, 1, 2, 3, 4]))
+    # def testSeveralDiscounts(self):
+    #     assert (8 + (8 * 2 * 0.95) == self.test_object.price([0, 0, 1]))
+    #     assert (2 * (8 * 2 * 0.95) == self.test_object.price([0, 0, 1, 1]))
+    #     assert ((8 * 4 * 0.8) + (8 * 2 * 0.95) == self.test_object.price([0, 0, 1, 2, 2, 3]))
+    #     assert (8 + (8 * 5 * 0.75) == self.test_object.price([0, 1, 1, 2, 3, 4]))
+    # def testEdgeCases(self):
+    #     assert (2 * (8 * 4 * 0.8) == self.test_object.price([0, 0, 1, 1, 2, 2, 3, 4]))
+    #     assert (3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8) ==  
+    #     self.test_object.price([0, 0, 0, 0, 0, 
+    #         1, 1, 1, 1, 1, 
+    #         2, 2, 2, 2, 
+    #         3, 3, 3, 3, 3, 
+    #         4, 4, 4, 4]))
 if __name__ == '__main__':
     unittest.main()
